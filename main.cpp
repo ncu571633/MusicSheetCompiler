3df094b85cb82f4a34535f10e366b119e84d52ab
ncu571633 <hechuanjun@msn.com>
2019-07-23 10:44:47 -0400

    new file:   main.cpp

---
+++ main.cpp
@@ -0,0 +1,11 @@
+
+
+int main(int arg, char* argv[])
+{
+    // pre-processing: convert input files to acceptable pgm file
+
+    // read pgm file into memory
+    netPbm::NetPbmImage* p = netPbm::ReadNetPbmImage("resource/p1.pbm");
+    
+    // transformation
+}
